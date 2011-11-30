1d9979ab53bade0a860bd088f1c5f82bb1287538
mitja.razpet@gmail.com <mitja.razpet@gmail.com@04d0b4fc-cc44-02b3-fe09-0347b9fc17d8>
2011-11-30 21:38:39 +0000

    osnovna verzija arhitekture

---
+++ trunk/src/application/WiseRSS/Business/RssChannelService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CrossCutting;
+using DataAccess;
+
+namespace Business
+{
+    public class RssChannelService
+    {
+
+        public RssChannel RssChannelSelect(int id)
+        {
+
+            //tukaj bo logika pri nekaterih 
+
+            return new RssChannelDataReader().RssChannelSelect(id);
+        }
+
+    }
+}
---
+++ trunk/src/application/WiseRSS/DataAccess/RssChannelDataReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CrossCutting;
+
+namespace DataAccess
+{
+    public class RssChannelDataReader
+    {
+        public RssChannel RssChannelSelect(int id)
+        {
+            RssChannel channel = new RssChannel();
+
+            //tukaj pride branje enega zapisa selektanega po idju
+
+            return channel;
+        }
+
+        public List<RssChannel> RssChannelSelectAll()
+        {
+            List<RssChannel> channelList = new List<RssChannel>();
+
+            //tukaj pride branje vseh zaapisov iz tabele rss_channels
+
+            return channelList;
+        }
+    }
+}
