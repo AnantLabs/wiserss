ccb66d205f163d6e6ca99af0c2b712bd2b0dbb49
mitja.razpet@gmail.com <mitja.razpet@gmail.com@04d0b4fc-cc44-02b3-fe09-0347b9fc17d8>
2011-11-30 21:38:39 +0000

    osnovna verzija arhitekture

---
+++ src/application/WiseRSS/CrossCutting/RssChannel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrossCutting
+{
+    public class RssChannel
+    {
+        int id { get; set; }
+        string title { get; set; }
+        string description { get; set; }
+        string link { get; set; }
+        DateTime published_date { get; set; }
+        bool favorite { get; set; }
+    }
+}
