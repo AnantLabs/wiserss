90cc2fde0201c502681569578d1ca95208e8446a
mitja.razpet@gmail.com <mitja.razpet@gmail.com@04d0b4fc-cc44-02b3-fe09-0347b9fc17d8>
2012-01-13 11:00:56 +0000

    dodani maili

--- trunk/src/application/WiseRSS/WiseRss/frmMain.Designer.cs
+++ trunk/src/application/WiseRSS/WiseRss/frmMain.Designer.cs
@@ -28,165 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-      System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Subscriptions", 0, 0);
-      System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Tags", 1, 1);
-      System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Bookmarks", 2, 2);
-      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.addToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-      this.newFeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-      this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.treeView1 = new System.Windows.Forms.TreeView();
-      this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.rchTxtContent = new System.Windows.Forms.RichTextBox();
-      this.menuStrip1.SuspendLayout();
-      this.SuspendLayout();
-      // 
-      // menuStrip1
-      // 
-      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Subscriptions", 0, 0);
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Tags", 1, 1);
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Bookmarks", 2, 2);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newFeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rchTxtContent = new System.Windows.Forms.RichTextBox();
+            this.btnMail = new DevExpress.XtraEditors.SimpleButton();
+            this.btnFacebook = new DevExpress.XtraEditors.SimpleButton();
+            this.btnTweeter = new DevExpress.XtraEditors.SimpleButton();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
             this.editToolStripMenuItem,
             this.viewToolStripMenuItem,
             this.helpToolStripMenuItem});
-      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-      this.menuStrip1.Name = "menuStrip1";
-      this.menuStrip1.Size = new System.Drawing.Size(830, 24);
-      this.menuStrip1.TabIndex = 1;
-      this.menuStrip1.Text = "menuStrip1";
-      // 
-      // fileToolStripMenuItem
-      // 
-      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(830, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addToolStripMenuItem1,
             this.saveAsToolStripMenuItem,
             this.exitToolStripMenuItem1});
-      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-      this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
-      this.fileToolStripMenuItem.Text = "&File";
-      // 
-      // addToolStripMenuItem1
-      // 
-      this.addToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // addToolStripMenuItem1
+            // 
+            this.addToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.newFeedToolStripMenuItem});
-      this.addToolStripMenuItem1.Name = "addToolStripMenuItem1";
-      this.addToolStripMenuItem1.Size = new System.Drawing.Size(162, 22);
-      this.addToolStripMenuItem1.Text = "&Add";
-      // 
-      // newFeedToolStripMenuItem
-      // 
-      this.newFeedToolStripMenuItem.Name = "newFeedToolStripMenuItem";
-      this.newFeedToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
-      this.newFeedToolStripMenuItem.Text = "&New Feed";
-      this.newFeedToolStripMenuItem.Click += new System.EventHandler(this.newFeedToolStripMenuItem_Click);
-      // 
-      // saveAsToolStripMenuItem
-      // 
-      this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-      this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-      this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
-      this.saveAsToolStripMenuItem.Text = "&Save As";
-      // 
-      // exitToolStripMenuItem1
-      // 
-      this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
-      this.exitToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-      this.exitToolStripMenuItem1.Size = new System.Drawing.Size(162, 22);
-      this.exitToolStripMenuItem1.Text = "&Exit";
-      // 
-      // editToolStripMenuItem
-      // 
-      this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-      this.editToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-      this.editToolStripMenuItem.Text = "&Edit";
-      // 
-      // viewToolStripMenuItem
-      // 
-      this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-      this.viewToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
-      this.viewToolStripMenuItem.Text = "&View";
-      // 
-      // helpToolStripMenuItem
-      // 
-      this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-      this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
-      this.helpToolStripMenuItem.Text = "&Help";
-      // 
-      // treeView1
-      // 
-      this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.addToolStripMenuItem1.Name = "addToolStripMenuItem1";
+            this.addToolStripMenuItem1.Size = new System.Drawing.Size(154, 22);
+            this.addToolStripMenuItem1.Text = "&Add";
+            // 
+            // newFeedToolStripMenuItem
+            // 
+            this.newFeedToolStripMenuItem.Name = "newFeedToolStripMenuItem";
+            this.newFeedToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.newFeedToolStripMenuItem.Text = "&New Feed";
+            this.newFeedToolStripMenuItem.Click += new System.EventHandler(this.newFeedToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.saveAsToolStripMenuItem.Text = "&Save As";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(154, 22);
+            this.exitToolStripMenuItem1.Text = "&Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.treeView1.Indent = 15;
-      this.treeView1.ItemHeight = 16;
-      this.treeView1.Location = new System.Drawing.Point(0, 27);
-      this.treeView1.Name = "treeView1";
-      treeNode1.ImageIndex = 0;
-      treeNode1.Name = "NodeSubscriptions";
-      treeNode1.SelectedImageIndex = 0;
-      treeNode1.Text = "Subscriptions";
-      treeNode2.ImageIndex = 1;
-      treeNode2.Name = "NodeTags";
-      treeNode2.SelectedImageIndex = 1;
-      treeNode2.Text = "Tags";
-      treeNode3.ImageIndex = 2;
-      treeNode3.Name = "NodeBookmarks";
-      treeNode3.SelectedImageIndex = 2;
-      treeNode3.Text = "Bookmarks";
-      this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            this.treeView1.Indent = 15;
+            this.treeView1.ItemHeight = 16;
+            this.treeView1.Location = new System.Drawing.Point(0, 27);
+            this.treeView1.Name = "treeView1";
+            treeNode1.ImageIndex = 0;
+            treeNode1.Name = "NodeSubscriptions";
+            treeNode1.SelectedImageIndex = 0;
+            treeNode1.Text = "Subscriptions";
+            treeNode2.ImageIndex = 1;
+            treeNode2.Name = "NodeTags";
+            treeNode2.SelectedImageIndex = 1;
+            treeNode2.Text = "Tags";
+            treeNode3.ImageIndex = 2;
+            treeNode3.Name = "NodeBookmarks";
+            treeNode3.SelectedImageIndex = 2;
+            treeNode3.Text = "Bookmarks";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
             treeNode1,
             treeNode2,
             treeNode3});
-      this.treeView1.PathSeparator = "/";
-      this.treeView1.ShowNodeToolTips = true;
-      this.treeView1.Size = new System.Drawing.Size(227, 409);
-      this.treeView1.TabIndex = 2;
-      this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
-      // 
-      // saveToolStripMenuItem
-      // 
-      this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-      this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-      this.saveToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
-      this.saveToolStripMenuItem.Text = "&Save As";
-      // 
-      // exitToolStripMenuItem
-      // 
-      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-      this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-      this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
-      this.exitToolStripMenuItem.Text = "&Exit";
-      // 
-      // richTextBox1
-      // 
-      this.rchTxtContent.Location = new System.Drawing.Point(233, 27);
-      this.rchTxtContent.Name = "richTextBox1";
-      this.rchTxtContent.Size = new System.Drawing.Size(597, 409);
-      this.rchTxtContent.TabIndex = 3;
-      this.rchTxtContent.Text = "";
-      // 
-      // frmMain
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(830, 448);
-      this.Controls.Add(this.rchTxtContent);
-      this.Controls.Add(this.treeView1);
-      this.Controls.Add(this.menuStrip1);
-      this.MainMenuStrip = this.menuStrip1;
-      this.Name = "frmMain";
-      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-      this.Text = "Wise RSS";
-      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-      this.menuStrip1.ResumeLayout(false);
-      this.menuStrip1.PerformLayout();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.treeView1.PathSeparator = "/";
+            this.treeView1.ShowNodeToolTips = true;
+            this.treeView1.Size = new System.Drawing.Size(227, 409);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.saveToolStripMenuItem.Text = "&Save As";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            // 
+            // rchTxtContent
+            // 
+            this.rchTxtContent.Location = new System.Drawing.Point(233, 53);
+            this.rchTxtContent.Name = "rchTxtContent";
+            this.rchTxtContent.Size = new System.Drawing.Size(597, 383);
+            this.rchTxtContent.TabIndex = 3;
+            this.rchTxtContent.Text = "";
+            // 
+            // btnMail
+            // 
+            this.btnMail.Image = global::WiseRss.Properties.Resources.mail_icon;
+            this.btnMail.Location = new System.Drawing.Point(233, 27);
+            this.btnMail.Name = "btnMail";
+            this.btnMail.Size = new System.Drawing.Size(75, 23);
+            this.btnMail.TabIndex = 4;
+            this.btnMail.Text = "e-mail";
+            this.btnMail.Click += new System.EventHandler(this.btnMail_Click);
+            // 
+            // btnFacebook
+            // 
+            this.btnFacebook.Image = global::WiseRss.Properties.Resources.facebook_icon;
+            this.btnFacebook.Location = new System.Drawing.Point(314, 27);
+            this.btnFacebook.Name = "btnFacebook";
+            this.btnFacebook.Size = new System.Drawing.Size(75, 23);
+            this.btnFacebook.TabIndex = 5;
+            this.btnFacebook.Text = "share fb";
+            this.btnFacebook.Click += new System.EventHandler(this.btnFacebook_Click);
+            // 
+            // btnTweeter
+            // 
+            this.btnTweeter.Image = global::WiseRss.Properties.Resources.facebook_icon;
+            this.btnTweeter.Location = new System.Drawing.Point(395, 27);
+            this.btnTweeter.Name = "btnTweeter";
+            this.btnTweeter.Size = new System.Drawing.Size(75, 23);
+            this.btnTweeter.TabIndex = 6;
+            this.btnTweeter.Text = "tweet";
+            this.btnTweeter.Click += new System.EventHandler(this.btnTweeter_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 448);
+            this.Controls.Add(this.btnTweeter);
+            this.Controls.Add(this.btnFacebook);
+            this.Controls.Add(this.btnMail);
+            this.Controls.Add(this.rchTxtContent);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wise RSS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -205,6 +241,9 @@
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
         private System.Windows.Forms.RichTextBox rchTxtContent;
+        private DevExpress.XtraEditors.SimpleButton btnMail;
+        private DevExpress.XtraEditors.SimpleButton btnFacebook;
+        private DevExpress.XtraEditors.SimpleButton btnTweeter;
     }
 }
 
